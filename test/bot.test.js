a78e7bc9b8207083c9786dc1399a77860664a155
Manuel Spigolon <behemoth89@gmail.com>
2020-08-19 22:26:50 +0200

    add user interface

--- test/bot.test.js
+++ test/bot.test.js
@@ -1,1 +1,28 @@
 'use strict'
+
+// TODO
+
+// nock to simulate the OAUTH2 response
+
+// {
+//   token: {
+//     access_token: '111111111111111111111111111111',
+//     expires_in: 604800,
+//     refresh_token: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+//     scope: 'identify',
+//     token_type: 'Bearer'
+//   }
+// }
+
+// {
+//   id: "111111111111111111",
+//   username: "Eomm",
+//   avatar: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+//   discriminator: "5262",
+//   public_flags: 0,
+//   flags: 0,
+//   email: "awesome@hello.com",
+//   verified: true,
+//   locale: "it",
+//   mfa_enabled: false
+// }
