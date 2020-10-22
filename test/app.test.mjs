bb04e3aaee59ae09023b0ed6bde628655918e82d
Manuel Spigolon <behemoth89@gmail.com>
2020-10-22 23:06:19 +0200

    test: oauth2 flow test

--- test/app.test.mjs
+++ test/app.test.mjs
@@ -16,6 +16,19 @@ const fakeTokenResponse = {
   }
 }
 
+const fakeUserProfile = {
+  id: '424242424242424242',
+  username: 'Foo Bar',
+  avatar: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+  discriminator: '5262',
+  public_flags: 0,
+  flags: 0,
+  email: 'mail@mail.com',
+  verified: true,
+  locale: 'it',
+  mfa_enabled: false
+}
+
 t.beforeEach(function (done, childTest) {
   // TODO move this config
   const config = {
@@ -51,20 +64,43 @@ t.test('the application redirect when 404', async t => {
   t.equal(res.statusCode, 302)
 })
 
-t.test('click on login', async t => {
-  const res = await t.context.server.inject('/auth/discord')
-  t.equal(res.statusCode, 302)
-  t.like(res.headers.location, 'discord.com')
-})
+t.test('OAUTH2 Code Flow', async t => {
+  await t.test('click on login', async t => {
+    const res = await t.context.server.inject('/auth/discord')
+    t.equal(res.statusCode, 302)
+    t.like(res.headers.location, 'discord.com')
 
-// TODO
-t.test('receive a callback', { todo: true }, async t => {
-  nock(oauth2.DISCORD_CONFIGURATION.tokenHost)
-    .post(oauth2.DISCORD_CONFIGURATION.tokenPath)
-    .reply(200, () => fakeTokenResponse)
+    const redirect = new URL(res.headers.location)
+    const oauthState = redirect.searchParams.get('state')
 
-  const res = await t.context.server.inject('/auth/discord/callback?code=ABC123')
-  t.equal(res.statusCode, 200)
+    await t.test('succesfull login', async t => {
+      const codeQueryParams = `code=ABC123&state=${oauthState}`
+      nock(oauth2.DISCORD_CONFIGURATION.tokenHost)
+        .post(oauth2.DISCORD_CONFIGURATION.tokenPath)
+        .reply(200, fakeTokenResponse)
+        .get('/api/users/@me')
+        .times(2)
+        .reply(200, fakeUserProfile)
+
+      const res = await t.context.server.inject(`/auth/discord/callback?${codeQueryParams}`)
+
+      t.equal(res.statusCode, 200)
+      t.like(res.payload, '<h5 class="card-title">Hi Foo Bar!</h5>', 'the page is loaded')
+      t.equals(res.cookies.length, 1, 'one session cookie must be returned')
+      t.equals(res.cookies[0].name, 'sessionid', 'the session id cookie')
+
+      const resCached = await t.context.server.inject({
+        method: 'GET',
+        url: '/auth/discord',
+        cookies: {
+          [res.cookies[0].name]: res.cookies[0].value
+        }
+      })
+
+      t.equal(resCached.statusCode, 200)
+      t.like(resCached.payload, '<h5 class="card-title">Hi Foo Bar!</h5>', 'the page is loaded')
+    })
+  })
 })
 
 t.test('receive a callback error', async t => {
@@ -72,16 +108,3 @@ t.test('receive a callback error', async t => {
   t.equal(res.statusCode, 500)
   t.like(res.payload, '<title>Error</title>', 'the error page is returned')
 })
-
-// {
-//   id: "111111111111111111",
-//   username: "Eomm",
-//   avatar: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-//   discriminator: "5262",
-//   public_flags: 0,
-//   flags: 0,
-//   email: "awesome@hello.com",
-//   verified: true,
-//   locale: "it",
-//   mfa_enabled: false
-// }
